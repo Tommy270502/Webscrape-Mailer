466c40ec24ea15a80c5128f504a84b5f20048387
Thomas S. Perri <87670767+Tommy270502@users.noreply.github.com>
2021-12-15 14:01:58 +0100

    Add files via upload

---
+++ mail webscraper.py
@@ -0,0 +1,34 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import smtplib
+from email.mime.multipart import MIMEMultipart
+
+news = ""
+r = requests.get("https://coinmarketcap.com/currencies/ethereum/")
+soup = BeautifulSoup(r.text, 'html.parser')
+result = soup.find('yourhtml', attrs = {'class':'yourclassname'})
+
+news += result.text
+
+gmail_user = 'yourmail@gmail.com' #Enter your gmail address
+gmail_password = 'yourpassword'   #Enter your Email pasword
+sent_from = gmail_user
+to = ['receivermail@mail.com']      #list of destination Email address
+
+#Email title
+msg = """Subject:
+"""
+#add news to email massage
+msg += news
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, msg)
+    server.close()
+
+    print ('Email sent!')
+except:
+    print ('Something went wrong...')
